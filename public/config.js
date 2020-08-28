698a31e0b5b175f01f8d1eb0f86dc3bfedf4e3c5
nohinon0810 <61536525+nohinon0810@users.noreply.github.com>
2020-08-29 00:56:09 +0900

    Add files via upload

---
+++ public/config.js
@@ -0,0 +1,16 @@
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBz0BPqgVDI-DvHIyAEvOOG7mmzfPtBgVQ",
+    authDomain: "tutorial-mahopro.firebaseapp.com",
+    databaseURL: "https://tutorial-mahopro.firebaseio.com",
+    projectId: "tutorial-mahopro",
+    storageBucket: "tutorial-mahopro.appspot.com",
+    messagingSenderId: "97992236408",
+    appId: "1:97992236408:web:b1530662423f4f9921fc8d",
+    measurementId: "G-4RQK4QVQPS"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+//  firebase.analytics();
+
